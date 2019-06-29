db94e9a2e42d5f0b1de72738473743aa5eacfc7f
redjojo <user@Suzy.app.byu.edu>
2019-06-29 15:34:08 -0600

    more fragments and layouts

---
+++ app/src/main/java/felsted/joanna/fmc/LoginFragment.java
@@ -0,0 +1,25 @@
+package felsted.joanna.fmc;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class LoginFragment extends Fragment {
+    private Button mSignIn;
+    private Button mRegister;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+        return v;
+    }
+}
---
+++ app/src/main/java/felsted/joanna/fmc/MapFragment.java
@@ -0,0 +1,23 @@
+package felsted.joanna.fmc;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MapFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_map, container, false);
+        return v;
+    }
+}

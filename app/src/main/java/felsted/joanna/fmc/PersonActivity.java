11f84f79b5a50a4f3e3b21b67bbfd95688190436
redjojo <user@Suzy.app.byu.edu>
2019-06-29 13:53:44 -0600

    all basic resources, layouts, and activities setup

--- app/src/main/java/felsted/joanna/fmc/PersonActivity.java
+++ app/src/main/java/felsted/joanna/fmc/PersonActivity.java
@@ -1,4 +1,12 @@
 package felsted.joanna.fmc;
 
-public class PersonActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PersonActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person);
+    }
 }
--- app/src/main/java/felsted/joanna/fmc/SearchActivity.java
+++ app/src/main/java/felsted/joanna/fmc/SearchActivity.java
@@ -1,4 +1,12 @@
 package felsted.joanna.fmc;
 
-public class SearchActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SearchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
 }

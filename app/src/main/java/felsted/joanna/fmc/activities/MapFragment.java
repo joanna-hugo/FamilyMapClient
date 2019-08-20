7e9d455934bdeb0fed04f3be4ece619222240b54
ukefan42 <joanna.hugo.junk@gmail.com>
2019-08-19 20:19:56 -0600

    maps working, but not fully functional

--- app/src/main/java/felsted/joanna/fmc/activities/MapFragment.java
+++ app/src/main/java/felsted/joanna/fmc/activities/MapFragment.java
@@ -1,181 +1,191 @@
 package felsted.joanna.fmc.activities;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.location.Location;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.google.android.gms.common.api.GoogleApiActivity;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.io.IOException;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import felsted.joanna.fmc.R;
-import felsted.joanna.fmc.ServerProxy;
-import felsted.joanna.fmc.model.event;
 
-public class MapFragment extends SupportMapFragment {
-    private String TAG = "MapFrag";
-    private static final String[] LOCATION_PERMISSIONS = new String[]{
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-    };
-    private static final int REQUEST_LOCATION_PERMISSIONS = 0;
+import static android.graphics.Color.BLUE;
+import static com.google.android.gms.maps.GoogleMap.MAP_TYPE_SATELLITE;
+import static com.google.android.gms.maps.model.BitmapDescriptorFactory.HUE_BLUE;
+import static com.google.android.gms.maps.model.BitmapDescriptorFactory.defaultMarker;
 
-    private GoogleApiClient mClient;
-    private GoogleMap mMap;
-    private Bitmap mMapImage;
-    private event[] Events;
-    private event mMapItem;
-    private Location mCurrentLocation;
+public class MapFragment extends Fragment {
 
-
-    public static MapFragment newInstance(){
-        return new MapFragment();
-    }
+    private GoogleMap map;
+    private TextView textView;
+    private MapView mapView;
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
 
-        mClient = new GoogleApiClient.Builder(getActivity())
-                .addApi(LocationServices.API)
-                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
-                    @Override
-                    public void onConnected(Bundle bundle) {
-                        getActivity().invalidateOptionsMenu();
-                    }
+        textView = (TextView) view.findViewById(R.id.mapText);
 
-                    @Override
-                    public void onConnectionSuspended(int i) {
+        mapView = view.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
 
-                    }
-                })
-                .build();
-        getMapAsync(new OnMapReadyCallback() {
+        mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
-                mMap = googleMap;
-                updateUI();
+                map = googleMap;
+                initMap();
             }
         });
+
+        return view;
+
     }
 
+    // really need to override all lifecycle methods
+    // to make the MapView work correctly
     @Override
-    public void onStart() {
-        super.onStart();
-
-        getActivity().invalidateOptionsMenu();
-        mClient.connect();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        mClient.disconnect();
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.map_fragment, menu);
+        inflater.inflate(R.menu.fragment_map, menu);
 
-        MenuItem searchItem = menu.findItem(R.id.action_locate);
-        searchItem.setEnabled(mClient.isConnected());
+        MenuItem searchItem = menu.findItem(R.id.search_button);
+
+        MenuItem filterItem = menu.findItem(R.id.filter_button);
+
+        MenuItem settingsItem = menu.findItem(R.id.settings_button);
     }
 
-    private void updateUI() {
-        if (mMap == null || mMapImage == null) {
-            return;
-        }
+    void initMap() {
 
-        LatLng itemPoint = new LatLng(mMapItem.getLatitude(), mMapItem.getLongitude());
-        LatLng myPoint = new LatLng(
-                mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+        centerMap();
+        zoomMap(10);
+        setMapType();
+        setClickListener();
+        zoomMap(2);
+        addMarkers();
+        setBounds();
+        setMarkerListener();
+        drawLines();
 
-        BitmapDescriptor itemBitmap = BitmapDescriptorFactory.fromBitmap(mMapImage);
-        MarkerOptions itemMarker = new MarkerOptions()
-                .position(itemPoint)
-                .icon(itemBitmap);
-        MarkerOptions myMarker = new MarkerOptions()
-                .position(myPoint);
-
-        mMap.clear();
-        mMap.addMarker(itemMarker);
-        mMap.addMarker(myMarker);
-
-
-        LatLngBounds bounds = new LatLngBounds.Builder()
-                .include(itemPoint)
-                .include(myPoint)
-                .build();
-
-        int margin = getResources().getDimensionPixelSize(R.dimen.map_inset_margin);
-        CameraUpdate update = CameraUpdateFactory.newLatLngBounds(bounds, margin);
-        mMap.animateCamera(update);
     }
 
-    private class getEvents extends AsyncTask<Location,Void,Void> {
-        private event mEvent;
-        private Bitmap mBitmap;
-        private Location mLocation;
+    void centerMap() {
+        LatLng byu = new LatLng(40.2518, -111.6493);
+        CameraUpdate update = CameraUpdateFactory.newLatLng(byu);
+        map.moveCamera(update);
+        map.addMarker(new MarkerOptions().position(byu));
+    }
 
-        @Override
-        protected Void doInBackground(Location... params) {
-            mLocation = params[0];
-            ServerProxy server = new ServerProxy();
-            try {
-                event[] items = server.getEvents("123MyAuth");
+    void zoomMap(float amount) {
+        CameraUpdate update = CameraUpdateFactory.zoomTo(amount);
+        map.moveCamera(update);
+    }
 
-                if (items.length== 0) {
-                    return null;
-                }
+    //    static final int mapType = MAP_TYPE_NORMAL;
+    static final int mapType = MAP_TYPE_SATELLITE;
 
-                mEvent = items[0];
+    void setMapType() {
+        map.setMapType(mapType);
+    }
 
-//                byte[] bytes = server.getUrlBytes(mEvent.getUrl()); //TODO
-//                mBitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-            } catch (IOException ioe) {
-                Log.i(TAG, "Unable to decode bitmap", ioe);
+    void setClickListener() {
+        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                textView.setText(latLng.toString());
+
             }
-            return null;
-        }
+        });
+    }
 
-        @Override
-        protected void onPostExecute(Void result) {
-            mMapImage = mBitmap;
-            mMapItem = mEvent;
-            mCurrentLocation = mLocation;
+    String[][] locations = {
+            {"Fairbanks", "64.8378", "-147.7164"},
+            {"Anchorage", "61.2181", "-149.9003"},
+            {"Sitka", "57.0531", "-135.3300"},
+            {"North Pole", "64.7511", "-147.3494"}
+    };
+    String getCity(String[] strings) {
+        return strings[0];
+    }
+    LatLng getLatLng(String[] strings) {
+        return new LatLng(Double.valueOf(strings[1]), Double.valueOf(strings[2]));
+    }
 
-            updateUI(); //TODO
+    void addMarkers() {
+        for (String[] strings : locations) {
+            addMarker(getCity(strings), getLatLng(strings));
         }
+    }
+
+    void addMarker(String city, LatLng latLng) {
+        MarkerOptions options =
+                new MarkerOptions().position(latLng).title(city)
+                        .icon(defaultMarker(HUE_BLUE));
+        Marker marker = map.addMarker(options);
+        marker.setTag(city);
+    }
+
+    void setBounds() {
+        LatLngBounds.Builder builder = LatLngBounds.builder();
+        for (String[] strings : locations) {
+            builder.include(getLatLng(strings));
+        }
+        LatLngBounds bounds = builder.build();
+        CameraUpdate update =
+                CameraUpdateFactory.newLatLngZoom(bounds.getCenter(), 5);
+        map.moveCamera(update);
+    }
+
+    void setMarkerListener() {
+        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                String city = (String)marker.getTag();
+                textView.setText(city);
+                return false;
+            }
+        });
+    }
+
+    void drawLines() {
+        LatLng lastCity = null;
+        for (String[] strings : locations) {
+            LatLng latLng = getLatLng(strings);
+            if (lastCity != null)
+                drawLine(lastCity, latLng);
+            lastCity = latLng;
+        }
+    }
+
+    static final float WIDTH = 10;  // in pixels
+    static final int color = BLUE;
+
+    void drawLine(LatLng point1, LatLng point2) {
+        PolylineOptions options =
+                new PolylineOptions().add(point1, point2)
+                        .color(color).width(WIDTH);
+        map.addPolyline(options);
     }
 
 }

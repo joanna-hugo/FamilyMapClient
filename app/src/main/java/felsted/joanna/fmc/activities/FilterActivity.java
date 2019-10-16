f4d437e916405c2945ca067b277963a04287e5d7
ukefan42 <joanna.hugo.junk@gmail.com>
2019-10-16 13:28:50 -0600

    Filter Activity working recycler

--- app/src/main/java/felsted/joanna/fmc/activities/FilterActivity.java
+++ app/src/main/java/felsted/joanna/fmc/activities/FilterActivity.java
@@ -1,6 +1,8 @@
 package felsted.joanna.fmc.activities;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,27 +12,35 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.joanzapata.iconify.IconDrawable;
+import com.joanzapata.iconify.fonts.FontAwesomeIcons;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import felsted.joanna.fmc.R;
-import felsted.joanna.fmc.model.FamilyModel;
 import felsted.joanna.fmc.model.Filters;
+import felsted.joanna.fmc.model.event;
+import felsted.joanna.fmc.model.person;
 
 public class FilterActivity extends AppCompatActivity {
-    private FamilyModel mFamilyModel;
-    private Filters filters = Filters.getInstance();
+//    private FamilyModel mFamilyModel;
+    private Filters mFilters = Filters.getInstance();
     private Context context;
 
-    private RecyclerView filterRecyclerView;
-    private FilterAdapter mFilterAdapter;
+    RecyclerView filterRecyclerView;
+    FilterAdapter mFilterAdapter;
+
 
     //TODO make layout pretty
     //TODO add all buttons and listeners
-    //TODO add recycler view(?) to dynamically show event_types
-        //TODO I should get the person activity running well then apply what I learn here
+    //DONE add recycler view(?) to dynamically show event_types
+        //DONE I should get the person activity running well then apply what I learn here
 
 
     @Override
@@ -38,88 +48,64 @@ public class FilterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
 
-        configureBackButton();
-
         filterRecyclerView = findViewById(R.id.filter_recycler);
         filterRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mFilterAdapter = new FilterAdapter(filters.getEvent_type_filters());
+        mFilterAdapter = new FilterAdapter(mFilters.getEvent_type_filters_names());
         filterRecyclerView.setAdapter(mFilterAdapter);
     }
 
-    private void configureBackButton(){
-        Button searchButton = findViewById(R.id.backButton);
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-    }
 
-    public FamilyModel getFamilyModel() {
-        return mFamilyModel;
-    }
+// -----------------------------------------------------------------------------------------------------
 
-    public void setFamilyModel(FamilyModel familyModel) {
-        mFamilyModel = familyModel;
-    }
+    private class FilterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView data;
+        private Switch showSwitch;
 
-    private class TypeFilterHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-        private TextView mTextView;
-        private Switch switchButton;
-        private Filters.Filter myFilter;
-
-        private TypeFilterHolder(LayoutInflater inflater, ViewGroup parent){
+        private FilterViewHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.filter_layout, parent, false));
 
-            mTextView = itemView.findViewById(R.id.filter_text);
-
-//            switchButton = new Switch(parent.getContext());
-            switchButton = (Switch) findViewById(R.id.filters_button);
-
-            switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            data = itemView.findViewById(R.id.filter_text);
+            showSwitch = itemView.findViewById(R.id.filters_button);
+            showSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    if (isChecked) {
-                        myFilter.setShow(true);
-                    } else {
-                        myFilter.setShow(false);
-                    }
+                     mFilters.setEvent_type_filters(data.getText().toString(), isChecked);
                 }
             });
 
-            itemView.setOnClickListener(this);
         }
 
-        private void bind(Filters.Filter filter){
-            myFilter = filter;
-            mTextView.setText(myFilter.getEvent_type());
+        private void bind(String given ){
+            StringBuilder sb = new StringBuilder(given + " Events \n FILTER BY " + given.toUpperCase() + " EVENTS");
+            data.setText(sb.toString());
+            showSwitch.setChecked(mFilters.getMappedFilter(given));
         }
 
         @Override
         public void onClick(View v){
-
+            Toast.makeText(v.getContext(),
+                    "you clicked a " + data + " filter", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private class FilterAdapter extends RecyclerView.Adapter<TypeFilterHolder> {
+    private class FilterAdapter extends RecyclerView.Adapter<FilterViewHolder> {
 
-        private List<Filters.Filter> myFilters;
+        private List<String> myFilters;
 
-        private FilterAdapter(List<Filters.Filter> filters) {
+        private FilterAdapter(List<String> filters) {
             myFilters = filters;
         }
 
         @Override
-        public TypeFilterHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        public FilterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
 
-            return new TypeFilterHolder(layoutInflater, parent);
+            return new FilterViewHolder(layoutInflater, parent);
         }
 
         @Override
-        public void onBindViewHolder(TypeFilterHolder holder, int position) {
-            Filters.Filter filter = myFilters.get(position);
-            holder.bind(filter);
+        public void onBindViewHolder(FilterViewHolder holder, int position) {
+
+            holder.bind(myFilters.get(position));
         }
 
         @Override
@@ -127,6 +113,7 @@ public class FilterActivity extends AppCompatActivity {
             return myFilters.size();
         }
     }
+
 
 
 
